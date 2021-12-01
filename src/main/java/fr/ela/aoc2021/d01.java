7440853ac20f7a02d625e68c8e032e9f923115b0
Etienne LACAZEDIEU <etienne-externe.lacazedieu@enedis.fr>
2021-12-01 22:19:21 +0100

    Day one, part two, simpler solution (with streaming from file)

--- src/main/java/fr/ela/aoc2021/d01.java
+++ src/main/java/fr/ela/aoc2021/d01.java
@@ -11,36 +11,51 @@ public class d01 {
 
     public static void main(String[] args) {
         try {
-            List<Integer> testValues = Files.lines(InputFileUtil.getPath(d01.class, "input-test")).map(Integer::valueOf).collect(Collectors.toList());
-            List<Integer> realValues = Files.lines(InputFileUtil.getPath(d01.class, "input")).map(Integer::valueOf).collect(Collectors.toList());
-            System.out.println("Test file increases (simple) : " + findIncreases(testValues.toArray(new Integer[0]), 1));
-            System.out.println("Real file increases (simple): " + findIncreases(realValues.toArray(new Integer[0]), 1));
             System.out.println("Test file increases : " + findIncreases(InputFileUtil.getPath(d01.class, "input-test"), 1));
             System.out.println("Real file increases : " + findIncreases(InputFileUtil.getPath(d01.class, "input"), 1));
+            System.out.println("Test file increases - Collector : " + collectIncreases(InputFileUtil.getPath(d01.class, "input-test"), 1));
+            System.out.println("Real file increases - Collector : " + collectIncreases(InputFileUtil.getPath(d01.class, "input"), 1));
 
-            System.out.println("Test file increases window (simple) : " + findIncreases(testValues.toArray(new Integer[0]), 3));
-            System.out.println("Real file increases window (simple) : " + findIncreases(realValues.toArray(new Integer[0]), 3));
             System.out.println("Test file increases window : " + findIncreases(InputFileUtil.getPath(d01.class, "input-test"), 3));
             System.out.println("Real file increases window : " + findIncreases(InputFileUtil.getPath(d01.class, "input"), 3));
+            System.out.println("Test file increases window - Collector : " + collectIncreases(InputFileUtil.getPath(d01.class, "input-test"), 3));
+            System.out.println("Real file increases window - Collector : " + collectIncreases(InputFileUtil.getPath(d01.class, "input"), 3));
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static int findIncreases(Path input, int windowSize) throws IOException {
+
+    public static int collectIncreases(Path input, int windowSize) throws IOException {
         return Files.lines(input).map(Integer::valueOf)
-                .collect(() -> new WindowCounter(windowSize), WindowCounter::add, (w, w1) -> {}).counter;
+                .collect(() -> new WindowCounter(windowSize), WindowCounter::add, (w, w1) -> {
+                }).counter;
     }
 
-    public static int findIncreases(Integer[] values, int windowSize) {
-        int count = 0;
-        for (int i = windowSize; i < values.length; i++) {
-            if (values[i] > values[i - windowSize]) {
-                count++;
-            }
+    public static int findIncreases(Path input, int windowSize) throws IOException {
+        return Files.lines(input).map(Integer::valueOf)
+                .collect(Collectors.toCollection(() -> new Counter(windowSize))).counter;
+    }
+
+
+    public static class Counter extends LinkedList<Integer> {
+        final int size;
+        int counter = 0;
+
+        public Counter(int size) {
+            this.size = size;
         }
-        return count;
+
+        @Override
+        public boolean add(Integer integer) {
+            if (size() == size && integer > pollLast()) {
+                counter++;
+            }
+            push(integer);
+            return true;
+        }
+
     }
 
 
