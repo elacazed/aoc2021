3152124e0844435b9ffcbbd8db16dc33d1aea357
Etienne LACAZEDIEU <etienne-externe.lacazedieu@enedis.fr>
2021-12-01 12:40:48 +0100

    Day one, part two

--- src/main/java/fr/ela/aoc2021/d01.java
+++ src/main/java/fr/ela/aoc2021/d01.java
@@ -3,83 +3,57 @@ package fr.ela.aoc2021;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Collections;
-import java.util.Set;
-import java.util.function.BiConsumer;
-import java.util.function.BinaryOperator;
-import java.util.function.Function;
-import java.util.function.Supplier;
-import java.util.stream.Collector;
+import java.util.LinkedList;
 import java.util.stream.Stream;
 
 public class d01 {
 
     public static void main(String[] args) {
         try {
-            System.out.println("Test file increases : " + findIncreases(InputFileUtil.getPath(d01.class, "input-test")));
-            System.out.println("Real file increases : " + findIncreases(InputFileUtil.getPath(d01.class, "input")));
+            System.out.println("Test file increases : " + findIncreases(InputFileUtil.getPath(d01.class, "input-test"), 1));
+            System.out.println("Real file increases : " + findIncreases(InputFileUtil.getPath(d01.class, "input"), 1));
+
+            System.out.println("Test file increases window : " + findIncreases(InputFileUtil.getPath(d01.class, "input-test"), 3));
+            System.out.println("Real file increases window : " + findIncreases(InputFileUtil.getPath(d01.class, "input"), 3));
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static int findIncreases(Path input) throws IOException {
+    public static int findIncreases(Path input, int windowSize) throws IOException {
         Stream<String> measurements = Files.lines(input);
         return measurements.map(Integer::valueOf)
-                .collect(new CountingIncreasesCollector());
+                .collect(() -> new WindowCounter(windowSize), WindowCounter::add, WindowCounter::merge).counter;
     }
 
 
-
-
-
-    public static class Counter {
+    public static class WindowCounter {
         int counter = 0;
-        int last = -1;
+        LinkedList<Integer> window;
+        final int windowSize;
 
-        public Counter() {
+        public WindowCounter(int windowSize) {
+            this.windowSize = windowSize;
             this.counter = 0;
+            this.window = new LinkedList<>();
         }
 
         public void add(int value) {
-            if (last != -1 && value > last) {
-                counter++;
+            if (window.size() == windowSize) {
+                int last = window.pollLast();
+                if (value > last) {
+                    counter++;
+                }
             }
-            last = value;
+            window.push(value);
         }
 
-        public Counter add(Counter other) {
+        public WindowCounter merge(WindowCounter other) {
             this.counter += other.counter;
             return this;
         }
     }
 
-    public static class CountingIncreasesCollector implements Collector<Integer, Counter, Integer> {
-
-        @Override
-        public Supplier<Counter> supplier() {
-            return Counter::new;
-        }
-
-        @Override
-        public BiConsumer<Counter, Integer> accumulator() {
-            return Counter::add;
-        }
-
-        @Override
-        public BinaryOperator<Counter> combiner() {
-            return Counter::add;
-        }
-
-        @Override
-        public Function<Counter, Integer> finisher() {
-            return c -> c.counter;
-        }
-
-        @Override
-        public Set<Characteristics> characteristics() {
-            return Collections.emptySet();
-        }
-    }
 
 }
