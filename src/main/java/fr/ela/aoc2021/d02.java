610ad03da0e15213afd70651d48fd4c05ca6983d
Etienne Lacazedieu <e.lacazedieu@groupeonepoint.com>
2021-12-03 08:20:27 +0100

    Day 2 with Consumer "composition"

--- src/main/java/fr/ela/aoc2021/d02.java
+++ src/main/java/fr/ela/aoc2021/d02.java
@@ -4,59 +4,56 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class d02 {
 
     public static void main(String[] args) {
         try {
-            System.out.println("Test Result : " + computePosition(InputFileUtil.getPath(d02.class, "input-test"), new Position()));
-            System.out.println("Test Real : " + computePosition(InputFileUtil.getPath(d02.class, "input"), new Position()));
+            Consumer<Position> testTrip = trip(InputFileUtil.getPath(d02.class, "input-test"));
+            Consumer<Position> realTrip = trip(InputFileUtil.getPath(d02.class, "input"));
 
-            System.out.println("Test Result 2 : " + computePosition(InputFileUtil.getPath(d02.class, "input-test"), new AimingPosition()));
-            System.out.println("Test Real 2 : " + computePosition(InputFileUtil.getPath(d02.class, "input"), new AimingPosition()));
+            System.out.println("Test Result : " + new Position().tripResult(testTrip));
+            System.out.println("Test Real : " + new Position().tripResult(realTrip));
+
+            System.out.println("Test Result 2 : " + new AimingPosition().tripResult(testTrip));
+            System.out.println("Test Real 2 : " + new AimingPosition().tripResult(realTrip));
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static int computePosition(Path input, Position pos) throws IOException {
+    public static Consumer<Position> trip(Path input) throws IOException {
         return Files.lines(input)
-                .reduce(pos, move, (p1, p2) -> p1).result();
+                .map(Direction::toConsumer)
+                .reduce(Consumer::andThen)
+                .orElseThrow();
     }
 
-    private static final BiFunction<Position, String, Position> move = (p, s) -> {
-            String[] values = s.split(" ");
-            Direction.valueOf(values[0]).moveFunction.accept(p, Integer.parseInt(values[1]));
-            return p;
-        };
-
     private static class Position {
-        int posX = 0;
-        int posY = 0;
-        int result() {return posX*posY;}
+        int posX, posY, aim = 0;
+        int tripResult(Consumer<Position> trip) {trip.accept(this); return posX*posY;}
         void down(int i) { posY +=i;}
-        void up(int i) { down(-1*i);}
-        void forward(int i) { posX +=i;}
+        void forward(int i) { posX +=i; posY += aim * i;}
     }
 
     private static class AimingPosition extends Position {
-        int realY = 0;
-        void forward(int i) { posX +=i; realY += i*posY;}
-        int result() {return posX*realY;}
+        void down(int i) { aim += i;}
     }
 
     public enum Direction {
         forward(Position::forward),
-        up(Position::up),
+        up((p,i) -> p.down(-1*i)),
         down(Position::down);
-
         final BiConsumer<Position, Integer> moveFunction;
-
         Direction(BiConsumer<Position, Integer> moveFunction) {
             this.moveFunction = moveFunction;
         }
+        private static Consumer<Position> toConsumer(String command) {
+            String[] values = command.split(" ");
+            return pos -> Direction.valueOf(values[0]).moveFunction.accept(pos, Integer.parseInt(values[1]));
+        }
     }
-
 }
