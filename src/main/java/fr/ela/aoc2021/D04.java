a8360e54cc9a621d16cd0de1d3156ae6c77e8410
Etienne Lacazedieu <e.lacazedieu@groupeonepoint.com>
2021-12-04 12:20:45 +0100

    D04 : part two

--- src/main/java/fr/ela/aoc2021/D04.java
+++ src/main/java/fr/ela/aoc2021/D04.java
@@ -2,8 +2,9 @@ package fr.ela.aoc2021;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -12,59 +13,69 @@ public class D04 extends AoC {
     @Override
     public void run() {
         Game test = new Game(list(getTestInputPath()));
-        Board testWinner = test.getWinner();
+        Board testWinner = test.getFirstWinner();
         System.out.println("Test Score part 1 : "+testWinner.score());
+        Board lastTestWinner = test.getLastWinner();
+        System.out.println("Test Score part 2 : "+lastTestWinner.score());
 
         Game real = new Game(list(getInputPath()));
-        Board realWinner = real.getWinner();
-        System.out.println("Test Score part 1 : "+realWinner.score());
+        Board realWinner = real.getFirstWinner();
+        System.out.println("Real Score part 1 : "+realWinner.score());
+        Board lastWinner = real.getLastWinner();
+        System.out.println("Real Score part 2 : "+lastWinner.score());
     }
 
     private static class Game {
-        final List<Integer> numbers;
+        final LinkedList<Integer> numbers;
         final List<Board> boards;
-        int lastNumber;
+        final LinkedList<Board> winnerBoards = new LinkedList<>();
 
         public Game(List<String> list) {
-            numbers = Arrays.stream(list.get(0).split(",")).map(Integer::parseInt).collect(Collectors.toList());
-            boards = new ArrayList<>();
+            numbers = Arrays.stream(list.get(0).split(",")).map(Integer::parseInt).collect(Collectors.toCollection(LinkedList::new));
+            boards = new LinkedList<>();
             for (int i = 2; i < list.size(); i += 5) {
                 boards.add(new Board(list.subList(i, i + 5)));
                 i++;
             }
         }
 
-        public List<Board> play(int number) {
+        public void play(int number) {
             Map<Boolean, List<Board>> map = boards.stream().collect(Collectors.groupingBy(b -> b.mark(number)));
-            return map.get(Boolean.TRUE);
+            var wins = map.get(Boolean.TRUE);
+            if (wins != null) {
+                winnerBoards.addAll(wins);
+                boards.removeAll(wins);
+            }
         }
 
-        public Board getWinner() {
-            Iterator<Integer> nbIterator = numbers.iterator();
+        public Board getFirstWinner() {
             int number = -1;
-            List<Board> winners = null;
-            while (winners == null) {
-                number = nbIterator.next();
-                winners = play(number);
+            while (winnerBoards.isEmpty() && !numbers.isEmpty()) {
+                number = numbers.removeFirst();
+                play(number);
             }
-            if (winners.size() > 1) {
-                throw new RuntimeException(winners.size()+" boards win!");
+            if (winnerBoards.size() > 1) {
+                throw new RuntimeException(winnerBoards.size()+" boards win!");
             } else {
-                lastNumber = number;
-                return winners.get(0);
+                return winnerBoards.get(0);
             }
         }
+
+        public Board getLastWinner() {
+            while (! numbers.isEmpty()) {
+                play(numbers.removeFirst());
+            }
+            return winnerBoards.getLast();
+        }
+
     }
 
     private record Position(int row, int col) {
     }
 
     private static class Board {
-
         private final Map<Integer, Position> positions;
-
         private final int[] colsSum, rowsSum;
-
         private int score;
 
         public Board(List<String> lines) {
