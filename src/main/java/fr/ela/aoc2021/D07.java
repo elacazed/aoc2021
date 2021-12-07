909b29469e862e633cc6a18a453c7c3ee0097922
Etienne Lacazedieu <e.lacazedieu@groupeonepoint.com>
2021-12-07 13:16:45 +0100

    D07 : part 1

--- src/main/java/fr/ela/aoc2021/D07.java
+++ src/main/java/fr/ela/aoc2021/D07.java
@@ -1,10 +1,43 @@
 package fr.ela.aoc2021;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class D07 extends AoC {
-
-
     @Override
     public void run() {
+        List<Integer> crabsPosition = Arrays.stream(readFile(getTestInputPath())
+                .split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        System.out.println("Solution Test partie 1 : "+getFuel(crabsPosition));
+
+        crabsPosition = Arrays.stream(readFile(getInputPath())
+                .split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        System.out.println("Solution partie 1 : "+getFuel(crabsPosition));
 
     }
+
+
+    private int getFuel(List<Integer> crabsPosition) {
+        crabsPosition.sort(Comparator.naturalOrder());
+        Integer min = crabsPosition.get(0);
+        Integer max = crabsPosition.get(crabsPosition.size() - 1);
+
+        int[] fuels = new int [max - min + 1];
+        for (int i = min; i <= max; i++) {
+            final int target = i;
+            fuels[i - min] = crabsPosition.stream()
+                    .mapToInt(p -> Math.abs(p - target))
+                    .sum();
+        }
+        int fuel = Arrays.stream(fuels).min().orElseThrow();
+        return fuel;
+    }
+
+
 }
