c4c06aa29976c7d80510a07c7784921900a6dc75
Etienne Lacazedieu <e.lacazedieu@groupeonepoint.com>
2021-12-07 09:23:46 +0100

    D02 : format

--- src/main/java/fr/ela/aoc2021/D02.java
+++ src/main/java/fr/ela/aoc2021/D02.java
@@ -25,23 +25,38 @@ public class D02 extends AoC {
 
     private static class Position {
         int posX, posY, aim = 0;
-        int tripResult(Consumer<Position> trip) {trip.accept(this); return posX*posY;}
-        void down(int i) { posY +=i;}
-        void forward(int i) { posX +=i; posY += aim * i;}
+
+        int tripResult(Consumer<Position> trip) {
+            trip.accept(this);
+            return posX * posY;
+        }
+
+        void down(int i) {
+            posY += i;
+        }
+
+        void forward(int i) {
+            posX += i;
+            posY += aim * i;
+        }
     }
 
     private static class AimingPosition extends Position {
-        void down(int i) { aim += i;}
+        void down(int i) {
+            aim += i;
+        }
     }
 
     public enum Direction {
         forward(Position::forward),
-        up((p,i) -> p.down(-1*i)),
+        up((p, i) -> p.down(-1 * i)),
         down(Position::down);
         final BiConsumer<Position, Integer> moveFunction;
+
         Direction(BiConsumer<Position, Integer> moveFunction) {
             this.moveFunction = moveFunction;
         }
+
         private static Consumer<Position> toConsumer(String command) {
             String[] values = command.split(" ");
             return pos -> Direction.valueOf(values[0]).moveFunction.accept(pos, Integer.parseInt(values[1]));
