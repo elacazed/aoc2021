6f618deb2d7ee3fbd949ab5b6c7ae690386ac81a
Etienne Lacazedieu <e.lacazedieu@groupeonepoint.com>
2021-12-09 23:09:18 +0100

    D09 : part two, saperlipopette.. L4enfermement dans une solution par firté intellectuelle ne mène pas bien loin! :p

--- src/main/java/fr/ela/aoc2021/D09.java
+++ src/main/java/fr/ela/aoc2021/D09.java
@@ -1,8 +1,14 @@
 package fr.ela.aoc2021;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class D09 extends AoC {
     @Override
@@ -11,29 +17,38 @@ public class D09 extends AoC {
         List<Point> lowPointsTest = testGrid.getLowPoints();
         System.out.println("Resultat Test Part 1 : " + lowPointsTest.stream().mapToInt(lp -> lp.value + 1).sum());
 
+        int value = lowPointsTest.stream().map(testGrid::getBassin)
+                .mapToInt(Set::size)
+                .boxed()
+                .sorted(Comparator.reverseOrder())
+                .limit(3)
+                .reduce(1, (x,y) -> x*y);
+        System.out.println("Résultat test PArt 2 : "+value);
+
+
         Grid grid = new Grid(list(getInputPath()));
         List<Point> lowPoints = grid.getLowPoints();
-        System.out.println("Resultat Resl Part 1 : " + lowPoints.stream().mapToInt(lp -> lp.value + 1).sum());
+        System.out.println("Resultat Reel Part 1 : " + lowPoints.stream().mapToInt(lp -> lp.value + 1).sum());
+        value = lowPoints.stream().map(grid::getBassin)
+                .mapToInt(Set::size)
+                .boxed()
+                .sorted(Comparator.reverseOrder())
+                .limit(3)
+                .reduce(1, (x,y) -> x*y);
+        System.out.println("Résultat reel Part 2 : "+value);
 
     }
 
+
     public class Grid {
-        final int[][] grid;
+        final List<String> grid;
         final int rows;
         final int cols;
 
         public Grid(List<String> values) {
             rows = values.size();
             cols = values.get(0).length();
-            grid = new int[rows][cols];
-            for (int i = 0; i < rows; i++) {
-                int[] row = new int[cols];
-                char[] rowValues = values.get(i).toCharArray();
-                for (int j = 0; j < cols; j++) {
-                    row[j] = Character.getNumericValue(rowValues[j]);
-                }
-                grid[i] = row;
-            }
+            grid = values;
         }
 
         public List<Point> getLowPoints() {
@@ -46,12 +61,12 @@ public class D09 extends AoC {
             return points;
         }
 
-        public int[] getRow(int row) {
-            return grid[row];
+        public String getRow(int row) {
+            return grid.get(row);
         }
 
-        public int getCol(int[] row, int col) {
-            return row[col];
+        public int getCol(String row, int col) {
+            return Character.getNumericValue(row.charAt(col));
         }
 
         public int getValue(int row, int col) {
@@ -60,31 +75,58 @@ public class D09 extends AoC {
 
         public Optional<Point> lowPoint(int row, int col) {
             int pointValue = getValue(row, col);
+            Point point = new Point(row, col, pointValue);
             if (pointValue == 9) {
                 return Optional.empty();
             }
             if (pointValue == 0) {
-                return Optional.of(new Point(row, col, pointValue));
+                return Optional.of(point);
             }
-            if (row > 0 && getValue(row -1, col) < pointValue) {
-                return Optional.empty();
+            if (point.adjacent(this).allMatch(p -> p.value > pointValue)) {
+                return Optional.of(point);
             }
-            if (row +1 < rows && getValue(row + 1, col) < pointValue) {
-                return Optional.empty();
-            }
-            int[] rowValue = getRow(row);
-            if (col > 0 && getCol(rowValue, col - 1) < pointValue) {
-                return Optional.empty();
-            }
-            if (col +1 < cols && getCol(rowValue, col+1) < pointValue) {
-                return Optional.empty();
-            }
-            return Optional.of(new Point(row, col, pointValue));
+            return Optional.empty();
         }
+
+        Point inGrid(Point p) {
+            if (p.row >= 0 && p.row < rows && p.col >= 0 && p.col < cols) {
+                return new Point(p.row, p.col, getValue(p.row, p.col));
+            } else {
+                return null;
+            }
+        }
+
+        public Set<Point> getBassin(Point low) {
+            return getBassin(low, new HashSet<>());
+        }
+
+
+        public Set<Point> getBassin(Point low, Set<Point> bassin) {
+            bassin.add(low);
+            for (Point adjacent : low.adjacent(this).filter(p -> p.value < 9).filter(p -> !bassin.contains(p)).collect(Collectors.toList())) {
+                bassin.addAll(getBassin(adjacent, bassin));
+            }
+            return bassin;
+        }
+
 
     }
 
     record Point(int row, int col, int value) {
+
+        Point(int row, int col) {
+            this(row, col, -1);
+        }
+
+        Stream<Point> adjacent(Grid grid) {
+            return Stream.of(new Point(row - 1, col), new Point(row, col - 1), new Point(row, col + 1), new Point(row + 1, col))
+                    .map(grid::inGrid)
+                    .filter(Objects::nonNull);
+        }
+
+        public String toString() {
+            return "[" + row + "," + col + "] (" + value + ")";
+        }
     }
 
 }
