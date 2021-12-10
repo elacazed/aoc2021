a154784dae6ee9de97ab375651f1d7a13247c7ef
Etienne Lacazedieu <e.lacazedieu@groupeonepoint.com>
2021-12-10 22:55:32 +0100

    D8 : with byte / word conversion to avoid sorting all words letters before translating.

--- src/main/java/fr/ela/aoc2021/D08.java
+++ src/main/java/fr/ela/aoc2021/D08.java
@@ -1,5 +1,6 @@
 package fr.ela.aoc2021;
 
+import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -13,21 +14,19 @@ public class D08 extends AoC {
 
     @Override
     public void run() {
-
-        List<Entry> testEntries = list(getTestInputPath(), Entry::new);
-        long easyNumbers = testEntries.stream().flatMap(Entry::streamOutputs).filter(D08::isEasyNuber).count();
-        System.out.println("Test result part 1 : " + easyNumbers);
-        List<Entry> entries = list(getInputPath(), Entry::new);
-        easyNumbers = entries.stream().flatMap(Entry::streamOutputs).filter(D08::isEasyNuber).count();
-        System.out.println("Real result part 1 : " + easyNumbers);
-
-        int result = decode(new Entry("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"));
-        System.out.println("Result : "+result);
-
-//        new BytesDecoder((new Entry("abcefg cf acdeg acdfg bcdf abdfg abdefg acf abcdefg abcdfg | cdfeb fcadb cdfeb cdbaf").inputs));
+        solve(getTestInputPath(), "Test");
+        solve(getInputPath(), "Real");
     }
 
-    private static final String sortString(String s) {
+
+    public void solve(Path path, String name) {
+        List<Entry> entries = list(path, Entry::new);
+        long easyNumbers = entries.stream().flatMap(Entry::streamOutputs).filter(D08::isEasyNuber).count();
+        System.out.println(name + " result part 1 : " + easyNumbers);
+        System.out.println(name + " result part 2 : " + entries.stream().mapToInt(D08::decode).sum());
+    }
+
+    private static String sortString(String s) {
         char[] chars = s.toCharArray();
         Arrays.sort(chars);
         return new String(chars);
@@ -38,10 +37,10 @@ public class D08 extends AoC {
         return length == 2 || length == 3 || length == 4 || length == 7;
     }
 
-    private class Entry {
+    private static class Entry {
 
-        private List<String> inputs;
-        private List<String> outputs;
+        private final List<String> inputs;
+        private final List<String> outputs;
 
         public Entry(String value) {
             String[] values = value.split(" \\| ");
@@ -66,24 +65,11 @@ public class D08 extends AoC {
     }
 
     private static class BytesDecoder {
-        final Map<String, Integer> realBytes = Map.of(
-                "abcefg", 0,
-                "cf", 1,
-                "acdeg", 2,
-                "acdfg", 3,
-                "bcdf", 4,
-                "abdfg", 5,
-                "abdefg", 6,
-                "acf", 7,
-                "abcdefg", 8,
-                "abcdfg", 9);
-
         final Map<Byte, Integer> permutation;
 
         public BytesDecoder(List<String> inputs) {
             permutation = findPermutation(inputs);
         }
-
 
         public byte toByte(char c) {
             byte b = 0;
@@ -99,47 +85,6 @@ public class D08 extends AoC {
             return b;
         }
 
-        public String fromByte(byte b) {
-            char[] chars = new char[7];
-            int i = 0;
-            for (char c : "abcdefg".toCharArray()) {
-                if ((b >> (7 - (int) c) & 1) != 0) {
-                    chars[i] = c;
-                    i++;
-                }
-            }
-            return new String(chars, 0, i + 1);
-        }
-
-        public byte encode(String word) {
-            byte b = 0;
-            for (char c : word.toCharArray()) {
-                b |= permutation.get(c);
-            }
-
-            return b;
-        }
-
-        /*
-               0 = abc efg
-             * 1 =   c  f
-               2 = a cde g
-               3 = a cd fg
-             * 4 =  bcd f
-               5 = ab d fg
-               6 = ab defg
-             * 7 = a c  f
-               8 = abcdefg
-             * 9 = abcd fg
-
-               a : 8
-                b : 6
-               c : 8
-               d : 7
-                e : 4
-                f : 9
-               g : 7
-               */
         private Map<Byte, Integer> findPermutation(List<String> entries) {
             Map<Byte, Integer> permutation = new HashMap<>();
             entries.sort(Comparator.comparingInt(String::length));
@@ -157,14 +102,12 @@ public class D08 extends AoC {
             byte b = toByte((char) frequencies.get(1).intValue());
             byte f = toByte((char) frequencies.get(2).intValue());
             // Combinaisons de caractères
-            byte cf = toByte(entries.get(0));
-
-            byte acf = toByte(entries.get(1));
-            byte bcdf = toByte(entries.get(2));
-            byte abcdefg = toByte(entries.get(9));
+            byte cf = toByte(entries.get(0)); // 1 (longueur 2)
+            byte acf = toByte(entries.get(1)); // 7 (longueur 3)
+            byte bcdf = toByte(entries.get(2)); // 4 (longueur 4)
+            byte abcdefg = toByte(entries.get(9)); // 8 (longueur 8)
 
             byte a = (byte) (acf ^ cf);
-
             byte c = (byte) (cf ^ f);
             byte d = (byte) (bcdf ^ f ^ c ^ b);
             byte g = (byte) (abcdefg ^ bcdf ^ a ^ e);
@@ -173,14 +116,15 @@ public class D08 extends AoC {
             permutation.put(cf, 1);
             permutation.put((byte) (a | c | d | e | g), 2);
             permutation.put((byte) (a | c | d | f | g), 3);
-            permutation.put((byte) bcdf, 4);
+            permutation.put(bcdf, 4);
             permutation.put((byte) (a | b | d | f | g), 5);
             permutation.put((byte) (a | b | d | e | f | g), 6);
             permutation.put(acf, 7);
-            permutation.put((byte) abcdefg, 8);
+            permutation.put(abcdefg, 8);
             permutation.put((byte) (a | b | c | d | f | g), 9);
 
             return permutation;
         }
+
     }
 }
