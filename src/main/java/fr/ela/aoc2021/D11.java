d0c8e760d36cbebd2d074244d1c3d0bdce8e1cc8
Etienne Lacazedieu <e.lacazedieu@groupeonepoint.com>
2021-12-11 16:37:36 +0100

    D11 : partie 1

---
+++ src/main/java/fr/ela/aoc2021/D11.java
@@ -0,0 +1,140 @@
+package fr.ela.aoc2021;
+
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.Stack;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class D11 extends AoC {
+
+    @Override
+    public void run() {
+        process(getTestInputPath(), "Test");
+        process(getInputPath(), "Real");
+    }
+
+    public void process(Path input, String name) {
+        Grid grid = new Grid(list(input));
+        System.out.println("Grille " + name + " construite");
+        System.out.println(grid);
+        System.out.println(name + " flashes : " + IntStream.range(0, 100).map(i -> grid.flash()).sum());
+
+    }
+
+    public class Grid {
+        final int[][] grid;
+        final int cols;
+        final int rows;
+
+        public Grid(List<String> input) {
+            rows = input.size();
+            cols = input.get(0).length();
+            grid = new int[rows][cols];
+
+            int row = 0;
+            for (String line : input) {
+                char[] chars = line.toCharArray();
+                for (int col = 0; col < cols; col++) {
+                    grid[row][col] = Character.getNumericValue(chars[col]);
+                }
+                row++;
+            }
+        }
+
+        public Stream<Point> rowStream(final int row) {
+            return IntStream.range(0, cols).mapToObj(col -> new Point(row, col));
+        }
+
+        public Stream<Point> pointStream() {
+            return IntStream.range(0, rows).mapToObj(this::rowStream).flatMap(Function.identity());
+        }
+
+        public String rowToString(final int row) {
+            return Arrays.stream(grid[row])
+                    .mapToObj(Integer::toString)
+                    .collect(Collectors.joining(""));
+        }
+
+        public String toString() {
+            StringBuilder sb = new StringBuilder("--------\n");
+            sb.append(IntStream.range(0, rows).mapToObj(this::rowToString).collect(Collectors.joining("\n")));
+            sb.append("\n-------");
+            return sb.toString();
+        }
+
+
+        public int inc(Point p) {
+            grid[p.row][p.col]++;
+            return grid[p.row][p.col];
+        }
+
+        public void set(Point p, int value) {
+            grid[p.row][p.col] = value;
+        }
+
+        public int get(Point p) {
+            return grid[p.row][p.col];
+        }
+
+        int flash() {
+            Set<Point> flashed = new HashSet<>();
+            Stack<Point> flashing = new Stack<>();
+            pointStream().filter(p -> inc(p) > 9).forEach(flashing::push);
+
+            Point current;
+            while (!flashing.isEmpty()) {
+                current = flashing.pop();
+
+                if (flashed.contains(current)) {
+                    continue;
+                }
+                flashed.add(current);
+                List<Point> adjacent = adjacent(current).collect(Collectors.toList());
+                adjacent.stream().filter(p -> !flashed.contains(p))
+                        .filter(p -> inc(p) > 9)
+                        .forEach(flashing::push);
+
+            }
+            flashed.forEach(p -> set(p, 0));
+            int nbFlashes = flashed.size();
+            return nbFlashes;
+        }
+
+        Stream<Point> adjacent(Point p) {
+            return Stream.of(
+                    getPoint(p, 1, 1),
+                    getPoint(p, 1, 0),
+                    getPoint(p, 1, -1),
+                    getPoint(p, 0, 1),
+                    getPoint(p, 0, -1),
+                    getPoint(p, -1, 1),
+                    getPoint(p, -1, 0),
+                    getPoint(p, -1, -1))
+                    .filter(Optional::isPresent)
+                    .map(Optional::get);
+        }
+
+        Optional<Point> getPoint(Point p, int dx, int dy) {
+            int row = p.row + dx;
+            int col = p.col + dy;
+            if ((0 <= row && row < rows) && (0 <= col && col < cols)) {
+                return Optional.of(new Point(row, col));
+            } else {
+                return Optional.empty();
+            }
+        }
+
+    }
+
+    record Point(int row, int col) {
+
+    }
+
+}
