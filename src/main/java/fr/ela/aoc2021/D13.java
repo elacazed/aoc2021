592f1054890bdbd6453112c4365e714a61ddab39
Etienne Lacazedieu <e.lacazedieu@groupeonepoint.com>
2021-12-13 17:08:23 +0100

    D12 : partie 2 - composition de fonction

--- src/main/java/fr/ela/aoc2021/D13.java
+++ src/main/java/fr/ela/aoc2021/D13.java
@@ -8,7 +8,9 @@ import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
 public class D13 extends AoC {
+
 
     @Override
     public void run() {
@@ -17,6 +19,7 @@ public class D13 extends AoC {
     }
 
     public void process(Path input, String name) {
+
         Map<Boolean, List<String>> linesSorted = stream(input)
                 .filter(s -> !s.isEmpty())
                 .collect(Collectors.groupingBy(l -> l.charAt(0) == 'f'));
@@ -26,34 +29,28 @@ public class D13 extends AoC {
                 .map(s -> new Point(Integer.parseInt(s[1]), Integer.parseInt(s[0])))
                 .collect(Collectors.toSet());
 
-        List<Function<Set<Point>, Set<Point>>> folds = linesSorted.get(Boolean.TRUE).stream()
+        List<Function<Point, Point>> folds = linesSorted.get(Boolean.TRUE).stream()
                 .map(this::toFold)
                 .collect(Collectors.toList());
 
-        grid = folds.get(0).apply(grid);
+        grid = fold(grid, folds.get(0));
         System.out.println(name + " grid has " + grid.size() + " visible dots after first fold");
-
-        Function<Set<Point>, Set<Point>> folder = folds.stream().skip(1).reduce(Function::andThen).orElseThrow();
-        grid = folder.apply(grid);
+        grid = fold(grid, folds.stream().skip(1).reduce(Function::andThen).orElseThrow());
         System.out.println(name + " Grid after all folding instructions : ");
         System.out.println(toString(grid));
     }
 
-    public Set<Point> foldOnY(Set<Point> set, int value) {
-        return set.stream().map(p -> p.row > value ? new Point((2 * value) - p.row, p.col) : p).collect(Collectors.toSet());
+    public Set<Point> fold(Set<Point> set, Function<Point, Point> folder) {
+        return set.stream().map(folder).collect(Collectors.toSet());
     }
 
-    public Set<Point> foldOnX(Set<Point> set, int value) {
-        return set.stream().map(p -> p.col > value ? new Point(p.row, (2 * value) - p.col) : p).collect(Collectors.toSet());
-    }
-
-    public Function<Set<Point>, Set<Point>> toFold(String value) {
+    public Function<Point, Point> toFold(String value) {
         String[] values = value.split("=");
-        int distance = Integer.parseInt(values[1]);
+        int axisPos = Integer.parseInt(values[1]);
         if (values[0].endsWith("x")) {
-            return s -> foldOnX(s, distance);
+            return p -> p.col > axisPos ? new Point(p.row, (2 * axisPos) - p.col) : p;
         } else {
-            return s -> foldOnY(s, distance);
+            return p -> p.row > axisPos ? new Point((2 * axisPos) - p.row, p.col) : p;
         }
     }
 
