1eec6a0365253ec493d371678fb63812ce803f89
Etienne Lacazedieu <e.lacazedieu@groupeonepoint.com>
2021-12-14 10:44:11 +0100

    D14 : Refacto.

--- src/main/java/fr/ela/aoc2021/D14.java
+++ src/main/java/fr/ela/aoc2021/D14.java
@@ -4,27 +4,27 @@ import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.List;
 import java.util.LongSummaryStatistics;
-import java.util.Map;
-import java.util.function.Function;
+import java.util.function.IntConsumer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class D14 extends AoC {
 
 
-    @Override
-    public void run() {
-        process(getTestInputPath(), "Test", 10);
-        process(getTestInputPath(), "Test", 40);
-        process(getInputPath(), "Real", 10);
-        process(getInputPath(), "Real", 40);
+    public void repeat(int times, IntConsumer action) {
+        IntStream.range(0, times).forEach(action);
     }
 
-    public void process(Path input, String name, int steps) {
+    @Override
+    public void run() {
+        process(getTestInputPath(), "Test");
+        process(getInputPath(), "Real");
+    }
+
+    public void process(Path input, String name) {
         List<String> list = list(input);
         String polymer = list.get(0);
-        Map<String, Rule> rules = new HashMap<>();
-        list.stream().skip(2)
-                .map(Rule::new)
-                .forEach(s -> rules.put(s.pair, s));
+        List<Rule> rules = list.stream().skip(2).map(Rule::new).collect(Collectors.toList());
 
         CountingMap<String> countPairs = new CountingMap<>();
         for (int i = 1; i < polymer.length(); i++) {
@@ -33,64 +33,37 @@ public class D14 extends AoC {
         }
         CountingMap<Character> countChars = new CountingMap<>();
         polymer.chars().forEach(i -> countChars.add((char) i, 1));
-
-
-        for (int i = 0; i < steps; i++) {
-            CountingMap<String> newPairs = new CountingMap<>();
-            CountingMap<Character> newChars = new CountingMap<>();
-            for (Rule rule : rules.values()) {
-                Counter c = countPairs.get(rule.pair);
-                if (c != null) {
-                    List<String> pairs = rule.result;
-                    for (String pair : pairs) {
-                        newPairs.computeIfAbsent(pair, p -> new Counter()).inc(c.count);
-                    }
-                    newChars.add(rule.addition, c.count);
-                }
-            }
-            countPairs = newPairs;
-            countChars.addAll(newChars);
-        }
-
+        repeat(10, i -> step(rules, countPairs, countChars));
         LongSummaryStatistics stats = countChars.values().stream().mapToLong(c -> c.count).summaryStatistics();
-        long min = stats.getMin();
-        long max = stats.getMax();
-        System.out.println(name + " Diff Part 1 : " + (max - min));
+        System.out.println(name + " Diff after 10 steps : " + (stats.getMax() - stats.getMin()));
+        repeat(30, i -> step(rules, countPairs, countChars));
+        stats = countChars.values().stream().mapToLong(c -> c.count).summaryStatistics();
+        System.out.println(name + " Diff after 40 steps : " + (stats.getMax() - stats.getMin()));
+        System.out.println(name + " total characters : " + stats.getSum());
     }
 
+    public void step(List<Rule> rules, CountingMap<String> pairs, CountingMap<Character> chars) {
+        CountingMap<String> newPairs = new CountingMap<>();
+        rules.forEach(r -> r.apply(pairs, chars, newPairs));
+        pairs.clear();
+        pairs.putAll(newPairs);
+    }
 
-    public class Counter {
+    public static class Counter {
         long count = 0;
 
         void inc(long x) {
             count = count + x;
         }
-        public String toString() {
-            return Long.toString(count);
+    }
+
+    public static class CountingMap<T> extends HashMap<T, Counter> {
+        public void add(T key, long count) {
+            super.computeIfAbsent(key, k -> new Counter()).inc(count);
         }
     }
 
-    public class CountingMap<T> extends HashMap<T, Counter> {
-
-        public Counter add(T key, Counter c) {
-            Counter c2 = super.computeIfAbsent(key, k -> new Counter());
-            c2.inc(c.count);
-            return c2;
-        }
-
-
-        public Counter add(T key, long count) {
-            Counter c2 = super.computeIfAbsent(key, k -> new Counter());
-            c2.inc(count);
-            return c2;
-        }
-
-        public void addAll(CountingMap<T> newChars) {
-            newChars.forEach((key, value) -> add(key, value));
-        }
-    }
-
-    public class Rule implements Function<String, List<String>> {
+    public static class Rule {
 
         final String pair;
         final char addition;
@@ -110,12 +83,12 @@ public class D14 extends AoC {
             this.result = List.of(new String(p1), new String(p2));
         }
 
-        @Override
-        public List<String> apply(String s) {
-            if (s.equals(pair)) {
-                return result;
+        public void apply(CountingMap<String> pairs, CountingMap<Character> chars, CountingMap<String> newPairs) {
+            Counter c = pairs.get(pair);
+            if (c != null) {
+                chars.add(addition, c.count);
+                result.forEach(v -> newPairs.add(v, c.count));
             }
-            return List.of(s);
         }
     }
 
